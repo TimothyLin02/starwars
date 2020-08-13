81ce9739de0f0484653f483a2080d92adf14d68f
Timothy Lin <tim.lin@rogers.com>
2020-08-13 14:04:48 -0400

    react app with bootstrap

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+//import Button from 'react-bootstrap/Button';
+//import { Button } from 'react-bootstrap';
 
 function Button(props) {
     console.log(props)
+
     return(
         <input
             type="button"
+            class="btn btn-dark"
+            //variant="outline-info"
             onClick={() => props.handleClick(props.src)}
-            value={props.val}
+            value={props.val}   
         />
     );
 }
